557c7e515f5578cfa81957698aba1d3c8a3d3f28
Martin Goberg <martingoberg@gmail.com>
2023-11-21 00:05:50 +0100

    Starta på figur

--- js/martin.js
+++ js/martin.js
@@ -4,10 +4,7 @@ import funksjoner from "./figures.js";
 // Trekant(x1 og y1, x2 og y2, x3 og y3)
 // Posisjoner på både x og y akse for alle 3 punktene.
 
-funksjoner.ellipse(50, 100, 40, 60, 0, 0, "red", "martinCanvas");
-
-funksjoner.firkant(150, 10, 125, 200, 45, "white", "martinCanvas");
-
-funksjoner.trekant(25, 250, 250, 150, 170, 25, "black", "martinCanvas");
-
-funksjoner.sirkel(70, 50, 15, "blue", "martinCanvas");
+funksjoner.firkant(120, 20, 60, 120, 45, "#29C5F6", "martinCanvas");
+funksjoner.firkant(120, 40, 60, 100, 45, "#3A9BDC", "martinCanvas");
+funksjoner.firkant(125, 45, 15, 15, 45, "#d2f1fe", "martinCanvas");
+funksjoner.firkant(120, 140, 60, 2, 45, "black", "martinCanvas");
